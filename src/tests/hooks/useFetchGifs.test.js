b65649af03fbec8a1c016ac01c780e5fceb8e8cc
Zenén Hernández Martínez <zenenhernandez85@gmail.com>
2020-11-05 21:40:59 -0500

    unit test for customHook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,6 +1,25 @@
-describe('Tests in useFetchGifs', () => {
-    test('should return the start state', () => {
-        
-    })
+import { renderHook } from "@testing-library/react-hooks";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+
+describe("Tests in useFetchGifs", () => {
+  test("should return the start state", async() => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs("Batman"));
+    const { data, loading } = result.current;
+
+    await waitForNextUpdate();
+
+    expect(data).toEqual([]);
+    expect(loading).toBe(true);
+  });
+
+  test('should return an Array of img and loading equal false', async() => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs("Batman"));
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+
     
-})
+    expect(data.length).toBe(3);
+    expect(loading).toBe(false);
+  })
+  
+});
